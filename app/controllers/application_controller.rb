3f18bf55ea844eddc6f03b23974461f1fd6bfd20
sakata-shougo <58379845+sakata-shougo@users.noreply.github.com>
2020-01-09 12:06:40 +0800

    finish login (#3)
    * finish login
    
    * add flash message
    
    * fix flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+ # By default Helpers are only available to the VIEW, so we must let the Controller access it too
+ include SessionsHelper
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  def new
+    # This is for the Login/Signin form
+  end
+  
+  def create
+    # Check if email exists in our database
+    # Check if password is correct for that email
+    user = User.find_by(email: params[:session][:email]) 
+      
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Successfully login"
+      # Sign the user in and redirect to the user's show page
+      sign_in user
+      redirect_to root_url
+    else
+      # Create an error message and re-render the signin form
+      flash[:danger] = "Invalid credentials"
+      redirect_to signin_url
+      # render 'new'
+      end
+    end
+  
+    def destroy
+      sign_out
+      flash[:success] = "Successfully logout"
+      redirect_to root_url
+    end
+end
