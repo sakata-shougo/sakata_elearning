18ad83b1930aa09d1a1f3c9cdc0de2baf4696d0e
坂田翔梧 <sakatashougo@sakatashouaogirinoMacBook-Air.local>
2020-01-10 11:44:27 +0800

    usersettings

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { minimum: 3, maximum: 65 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, length: { minimum: 3 }
+  validates :password, length: { minimum: 3 },allow_nil:true
+
+  mount_uploader :avatar, AvatarUploader
 end
