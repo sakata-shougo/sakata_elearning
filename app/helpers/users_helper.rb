c616904425dacd8f2fb9c6d6716e875056f5b97e
坂田翔梧 <sakatashougo@sakatashouaogirinoMacBook-Air.local>
2020-01-13 09:40:03 +0800

    finish

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def user_avatar(user,classess)
+   @avatar = user.avatar
+   if @avatar.present?
+      @avatar_user = image_tag(@avatar.url, class: classess)
+   else
+       @avatar_user = image_tag("default.png", class: classess)
+   end
+  end
 end
