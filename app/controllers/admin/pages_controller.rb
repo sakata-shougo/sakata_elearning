400359ec99f936931540f4f3f4f27e15511b1cb6
坂田翔梧 <sakatashougo@sakatashouaogirinoMacBook-Air.local>
2020-01-15 10:18:42 +0800

    finish adminhome

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,4 +1,7 @@
 class Admin::PagesController < ApplicationController
+  before_action :logged_in_user
+  before_action :admin_user
+  
   def home
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
  # By default Helpers are only available to the VIEW, so we must let the Controller access it too
  include SessionsHelper
+
+ #applicationControllerは、異なるcontroller間で使える。
+
+ def logged_in_user　#
+  unless signed_in?
+    flash[:danger] = "Please log in"
+    redirect_to signin_url
+  end     
+ end
+
+
+
+ def admin_user #
+    unless current_user.admin?
+        flash[:danger] = "You are not authorized"
+        redirect_to root_url
+    end   
+ end
 end
